ba795a79d09103b23735d9a265e7d63dcd7593ff
jheikkinen <56417363+jheikkinen@users.noreply.github.com>
2023-12-03 18:36:29 -0500

    Delete Stored Procedure Update Truck Mileage.sql

--- Stored Procedure Update Truck Mileage.sql
+++
@@ -1,5 +0,0 @@
-Create Procedure TrailerMileageUpdate (@TrailerNumber int, @TrailerMileage int)
-AS
-UPDATE TrailerInformation
-SET TrailerMileage = @TrailerMileage
-Where TrailerNumber = @TrailerNumber;
