1193515f75bfc6a96d4558982fc4d0bced4dcb98
jheikkinen <56417363+jheikkinen@users.noreply.github.com>
2023-12-03 18:36:58 -0500

    Delete Stored Procedure_Week 6 Query Truck Haul report.sql

--- Stored Procedure_Week 6 Query Truck Haul report.sql
+++
@@ -1,8 +0,0 @@
-CREATE Procedure TruckHaulReport(@TruckNumber int, @DateHaulBegan1 date, @DateHaulBegan2 date)
-AS
-select TruckInformation.*, HaulRecord.* 
-	From TruckInformation
-	join HaulRecord
-	on HaulRecord.TruckNumber = TruckInformation.TruckNumber 
-	Where TruckInformation.TruckNumber = @TruckNumber AND (DateHaulBegan BETWEEN @DateHaulBegan1 AND @DateHaulBegan2)
-	Order by DateHaulBegan;
