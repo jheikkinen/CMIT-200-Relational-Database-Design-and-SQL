7404692d0e82a518c5d96eb5ff7e1c424be9f115
jheikkinen <56417363+jheikkinen@users.noreply.github.com>
2023-12-03 18:36:50 -0500

    Delete Stored Procedure_Week 6 Query Haul Record.sql

--- Stored Procedure_Week 6 Query Haul Record.sql
+++
@@ -1,5 +0,0 @@
-CREATE Procedure QuickHaulReport(@TruckNumber int, @DateHaulBegan1 date, @DateHaulBegan2 date)
-AS
-select * From HaulRecord
-	Where TruckNumber = @TruckNumber AND (DateHaulBegan BETWEEN @DateHaulBegan1 AND @DateHaulBegan2)
-	Order by DeliveryDate;
